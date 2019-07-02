939a84427a7c5f43bea66b4e1d34ce3e733016c9
Thierry Poirine <tpoirine@gmail.com>
2019-07-02 10:09:16 -1000

    Ajout de devise et bootstrap + localisation fr pour authentification et vues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # méthode de devise (toutes les actions de l'UI nécessiteront qu'un utilisateur soit connecté)
+  
 end
